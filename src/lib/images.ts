91c03a51e09ee79189698fdc322b5ca8a6794355
developerkhurrana <214771352+ethnicsbyaravalli@users.noreply.github.com>
2025-09-17 17:30:22 +0530

    great success

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -5,6 +5,6 @@ export default {
         Peach silk Embroidered kaftan: "https://ekohum.com/cdn/shop/files/DSC7093.jpg?v=1717779598",
         White Dobby Short Kurta with  Pink Ikhat Pant Co-Ord Set: "https://ekohum.com/cdn/shop/files/DSC7391.jpg?v=1717779241",
         Lavender Cotton Gathered Top: "https://ekohum.com/cdn/shop/files/DSC6478.jpg?v=1717779121",
-        White Dobby Short Kurta with  Blue Ikhat Pant Co-Ord Set: https://ekohum.com/cdn/shop/files/DSC7338_968d4f2c-bf38-4f10-b342-542a7848c8fd.jpg?v=1717779252,
+        "White Dobby Short Kurta with  Blue Ikhat Pant Co-Ord Set": "https://ekohum.com/cdn/shop/files/DSC7338_968d4f2c-bf38-4f10-b342-542a7848c8fd.jpg?v=1717779252",
         
     }
